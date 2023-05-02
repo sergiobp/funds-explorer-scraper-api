121637bc5fb0106b2c438942e777ccb011da539b
jairo <jairo.araujo.silva@gmail.com>
2023-05-02 19:25:19 -0300

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.10-slim
 ENV TZ="America/Sao_Paulo"
 ENV PYTHONUNBUFFERED=1
-COPY dist/*.whl /tmp
-RUN pip install /tmp/*.whl
+RUN mkdir -p /tmp/app
+WORKDIR /tmp/app
+COPY . .
+RUN pip install .
 CMD ["python", "-m", "funds_explorer_scraper_api"]
--- src/funds_explorer_scraper_api/__main__.py
+++ src/funds_explorer_scraper_api/__main__.py
@@ -1,27 +1,4 @@
-import asyncio
-import signal
-from .api import make_api_server
-
-
-async def _main():
-    tasks = asyncio.gather(make_api_server())
-
-    def _shutdown(*args):
-        tasks.cancel()
-
-    loop = asyncio.get_event_loop()
-    loop.add_signal_handler(signal.SIGTERM, _shutdown)
-    loop.add_signal_handler(signal.SIGINT, _shutdown)
-    await tasks
-
-
-def main():
-    try:
-        asyncio.run(_main())
-    except Exception as error:
-        print("Unexpected error has ocurred.")
-        print(repr(error))
-
-
 if __name__ == "__main__":
+    from ._main import main
+
     main()
